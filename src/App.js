741ab746e85aaff04710a33dcd50f317e66e75a7
Anmol Dash <anmold@sas.upenn.edu>
2025-02-22 01:01:27 -0500

    t4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import Application from './pages/Application';
@@ -7,12 +7,14 @@ import GetInvolved from './pages/GetInvolved';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/apply" element={<Application />} />
-      <Route path="/get-involved" element={<GetInvolved />} />
-    </Routes>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/apply" element={<Application />} />
+        <Route path="/get-involved" element={<GetInvolved />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
